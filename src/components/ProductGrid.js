3de79e3c57d6462f2ee59d324ee4602664d54f0a
afif14 <mafifsolahudin@gmail.com>
2024-10-16 16:17:23 +0700

    fix: responsive input search and sorting, handle empty product

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -38,38 +38,49 @@ const ProductGrid = ({ products, searchTerm, handleSearch, wishlist, toggleWishl
 
     return (
         <div className='px-4' >
-            <div className="flex justify-between items-center mb-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center mb-6">
                 <input
                     type="text"
                     placeholder="Search products..."
                     value={searchTerm}
                     onChange={handleSearch}
-                    className="w-2/3 p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent dark:text-black"
                 />
-                <SortingDropdown sortBy={sortBy} setSortBy={setSortBy} />
+                <div className="w-full flex justify-end">
+                    <SortingDropdown sortBy={sortBy} setSortBy={setSortBy} />
+                </div>
             </div>
             {isLoading ? (
                 <SkeletonLoader />
             ) : (
                 <>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                        {currentProducts.map(product => (
-                            <ProductCard
-                                key={product.id}
-                                product={product}
-                                isWishlisted={wishlist.includes(product.id)}
-                                toggleWishlist={toggleWishlist}
-                            />
-                        ))}
-                    </div>
+                    {currentProducts.length === 0 ? (
+                        <div className=" flex justify-center py-6 text-center text-gray-500">
+                            <p>Product is empty.</p>
+                        </div>
+                    ) : (
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
 
-                    <Pagination
-                        currentPage={currentPage}
-                        totalPages={totalPages}
-                        onPageChange={handlePageChange}
-                    />
+                            {currentProducts.map(product => (
+                                <ProductCard
+                                    key={product.id}
+                                    product={product}
+                                    isWishlisted={wishlist.includes(product.id)}
+                                    toggleWishlist={toggleWishlist}
+                                />
+                            ))}
+
+                        </div>
+                    )}
+
+                    {currentProducts.length !== 0 && (
+                        <Pagination
+                            currentPage={currentPage}
+                            totalPages={totalPages}
+                            onPageChange={handlePageChange}
+                        />
+                    )}
                 </>
-
             )}
         </div>
     );
